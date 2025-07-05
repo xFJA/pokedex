eb286b5da9473b21719c9dc7ee53b8ff41bd26b1
xfja <franjimenezaguilera@gmail.com>
2025-07-05 20:34:52 +0200

    feat: add React Query

--- src/features/pokemon-list/hooks/usePokemonList.ts
+++ src/features/pokemon-list/hooks/usePokemonList.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import type { Pokemon } from '../types/pokemon';
 import { pokemonApi } from '../api/pokemonApi';
 
@@ -10,40 +10,14 @@ interface UsePokemonListResult {
 }
 
 export function usePokemonList(limit = 20, offset = 0): UsePokemonListResult {
-  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [total, setTotal] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    let isMounted = true;
-    setIsLoading(true);
-    setError(null);
-
-    pokemonApi
-      .getPokemonList(limit, offset)
-      .then(({ paginatedPokemons, total }) => {
-        if (isMounted) {
-          setPokemonList(paginatedPokemons);
-          setTotal(total);
-        }
-      })
-      .catch(err => {
-        if (isMounted) setError(err instanceof Error ? err : new Error('Failed to fetch Pokemons'));
-      })
-      .finally(() => {
-        if (isMounted) setIsLoading(false);
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [limit, offset]);
-
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['pokemonList', limit, offset],
+    queryFn: () => pokemonApi.getPokemonList(limit, offset),
+  });
   return {
-    pokemonList,
-    total,
+    pokemonList: data?.paginatedPokemons ?? [],
+    total: data?.total ?? 0,
     isLoading,
-    error,
+    error: error as Error | null,
   };
 }
